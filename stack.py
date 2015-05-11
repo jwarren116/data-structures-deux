5d7057bc34e10070a5b3f825618e87ad91fcbfea
James Warren <jwarren116@gmail.com>
2015-05-10 17:40:39 -0700

    Add attributes to Stack and Item classes

--- stack.py
+++ stack.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
 '''Implementation of a simple stack data structure.
-The stack has push, pop, and peek methods.
+The stack has push, pop, and peek methods. Items in the stack have a value,
+and next_item attribute. The stack has a top attribute.
 '''
 
 
 class Item(object):
-    def __init__(self):
-        pass
+    def __init__(self, value, next_item=None):
+        self.value = value
+        self.next_item = next_item
 
 
 class Stack(object):
-    def __init__(self):
-        pass
+    def __init__(self, top=None):
+        self.top = top
