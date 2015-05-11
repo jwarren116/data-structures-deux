744c66ea9b8f66d7617034829218c61f147fce25
James Warren <jwarren116@gmail.com>
2015-05-10 17:36:05 -0700

    Initial commit and outline of stack module

---
+++ stack.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+'''Implementation of a simple stack data structure.
+The stack has push, pop, and peek methods.
+'''
+
+
+class Item(object):
+    def __init__(self):
+        pass
+
+
+class Stack(object):
+    def __init__(self):
+        pass
