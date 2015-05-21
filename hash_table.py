361cf82bb3a659b711ba11078421a7be8ea06dc8
James Warren <jwarren116@gmail.com>
2015-05-21 11:47:26 -0700

    Initial creation of hash table module

---
+++ hash_table.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+'''Implementation of a simple hash table.
+The table has `hash`, `get` and `set` methods.
+The hash function uses a very basic hash algorithm to insert the value 
+into the table.
+'''
