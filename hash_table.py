b634790bf331521b2e7fae0609e79c1910c87f2e
James Warren <jwarren116@gmail.com>
2015-05-21 11:51:20 -0700

    Pencil out methods and classes

--- hash_table.py
+++ hash_table.py
@@ -5,3 +5,22 @@ The table has `hash`, `get` and `set` methods.
 The hash function uses a very basic hash algorithm to insert the value 
 into the table.
 '''
+
+
+class HashItem(object):
+    def __init__(self):
+        pass
+
+
+class Hash(object):
+    def __init__(self):
+        pass
+
+    def hash(self):
+        pass
+
+    def get(self):
+        pass
+
+    def set(self):
+        pass
